44c2813b5358cc754bebd15d128e56f0f2ec2130
ishitap11 <ishitap13@gmail.com>
2025-06-22 19:08:08 +0200

    added MarkowitzModel intial implementation and related documentaion

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # quantitative-finance-and-algo-trading
 Python repository to build scripts relevant for Quantitative Finance and useful for Algorithmic Trading.
+
+# Markowitz Model
+Deals with generating 10000 random portoflios for stocks = ['AAPL', 'AMZN', 'GE', 'DB', 'TSLA', 'WMT'].
+
+Stock historical data has been pulled from yahoo finance.
+
+![screenshot](imageDir/stocks.png)
+
+Calculates their expected return and expected volatility to find the optimal portfolio on the efficient frontier.
+
+![screenshot](imageDir/frontier.png)
---
+++ markowitzmodel/MarkowitzModel.py
@@ -0,0 +1,87 @@
+import numpy as np
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.optimize as opt
+
+#average number of trading days in a year
+NUM_TRADING_DAYS = 252
+NUM_PORTFOLIOS = 10000
+# defining the stock names for the universe
+stocks = ['AAPL', 'AMZN', 'GE', 'DB', 'TSLA', 'WMT']
+
+# Date range for the analysis
+start_date = '2012-01-01'
+end_date = '2017-01-01'
+
+def download_data():
+    # dictionary of stock name (key) - closing prices of each day (values)
+    stock_data = {}
+    for stock in stocks:
+        ticker = yf.Ticker(stock)
+        stock_data[stock] = ticker.history(start=start_date, end=end_date)['Close']
+
+    return pd.DataFrame(stock_data)
+
+def show_data(data):
+    data.plot(figsize=(10, 5))
+    plt.show()
+
+def calculate_return(data):
+    #Natural Log helps normalize the dataset since each stock has different volatility
+    log_return = np.log(data/data.shift(1)) #S(t+1)/S(t)
+    # the data.shift(n) function shifts the array of values by n places. so value at data[k] --> data[k+n]
+    return log_return[1:] #leave out row 0 as it will be not defined since S[-1] does not exist
+
+def show_statistics(returns):
+    #gives mean annual return = daily mean * number of trading days
+    print(returns.mean()*NUM_TRADING_DAYS)
+    #covariance matrix
+    print(returns.cov()*NUM_TRADING_DAYS)
+
+def get_expected_mean(returns, weights):
+    return np.sum(returns.mean()*weights)*NUM_TRADING_DAYS
+
+def get_expected_risk(returns, weights):
+    return np.sqrt(np.dot(weights.T, np.dot(returns.cov()*NUM_TRADING_DAYS, weights)))
+
+def show_expected_mean_variance(returns, weights):
+    # expected return
+    portfolio_returns = get_expected_mean(returns, weights)
+    print("expected return of portfolio: ", portfolio_returns)
+    # expected risk using covariance
+    portfolio_volatility_risk = get_expected_risk(returns, weights)
+    print("expected volatility of portfolio: ", portfolio_volatility_risk)
+
+def generate_portfolio(returns):
+    portfolio_weights = []
+    portfolio_returns = []
+    portfolio_volatility_risk = []
+
+    for _ in range(NUM_PORTFOLIOS):
+        weights = np.random.random(len(stocks))
+        weights /= np.sum(weights) # make sure weights add up to 1 (100%)
+        portfolio_weights.append(weights)
+        #expected return for each portfolio
+        portfolio_returns.append(get_expected_mean(returns, weights))
+        #expected risk for each portfolio
+        portfolio_volatility_risk.append(get_expected_risk(returns, weights))
+
+    return np.array(portfolio_weights), np.array(portfolio_returns), np.array(portfolio_volatility_risk)
+
+def show_portfolio(returns, volatility_risk):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(volatility_risk, returns, c=returns/volatility_risk, marker='o') #c --> color based on sharpe ratio
+    plt.grid(True)
+    plt.xlabel('Expected Volatility')
+    plt.ylabel('Expected Return')
+    plt.colorbar(label='Sharpe Ratio')
+    plt.show()
+
+if __name__ == "__main__":
+    data = download_data()
+    show_data(data)
+    daily_returns = calculate_return(data)
+    show_statistics(daily_returns)
+    portfolio_weights, return_means, volatility_risk = generate_portfolio(daily_returns)
+    show_portfolio(return_means, volatility_risk)
